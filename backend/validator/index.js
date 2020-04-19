5daaaf5943194814c8ce798f6b24c6a24d789bca
Tyree adams <tyree@tyrees-mbp.lan>
2020-04-19 18:04:57 -0400

    fixed customer schema

--- backend/validator/index.js
+++ backend/validator/index.js
@@ -1,5 +1,6 @@
 exports.userSignupValidator = (req, res, next) => {
-    req.check('name', 'Name is required').notEmpty();
+    req.check('first_name', 'Name is required').notEmpty();
+    req.check('last_name', 'Name is required').notEmpty();
     req.check('email', 'Email must be between 3 to 32 characters')
         .matches(/.+\@.+\..+/)
         .withMessage('Email must contain @')
