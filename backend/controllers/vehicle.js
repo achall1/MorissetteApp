da0a8ebecb32b3328a92b8a79f48c6a02c766011
amanuel <achall1@students.towson.edu>
2020-05-04 16:40:13 -0400

    image route done

--- backend/controllers/vehicle.js
+++ backend/controllers/vehicle.js
@@ -26,6 +26,15 @@ exports.read = (req, res) =>
     req.vehicle.image = undefined
     return res.json(req.vehicle)
 }
+//retrun vehicle image  to frontend
+exports.image = (req, res, next) => {
+    if (req.vehicle.image.data) {
+        res.set('Content-Type',req.vehicle.image.contentType);//"image/jpeg; charset=UTF-8" 
+        return res.send(req.vehicle.image.data);
+    }
+    next();
+};
+
 
 // delete a vehicle using adminiD and vehicle id
 exports.remove = (req, res) =>
@@ -96,6 +105,122 @@ if(!make || !model|| !mileage||  !price|| !vin|| !invintoryCount)
         })
     })
 }
+
+
+/**
+ * find all cars and return them byorder using model/make/year or any other fiels
+ * the query string is like below
+ * by model = /vehicles?sortBy=model&order=desc&limit=5
+ * by make = /vehicles?sortBy=make&order=desc&limit=5
+ * by year = /vehicles?sortBy=year&order=desc&limit=5
+ * if there is no parma then all vehicles are returned
+ */
+
+exports.findCar = (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    let limit = req.query.limit ? parseInt(req.query.limit) :100;
+
+    Vehicle.find()
+        .select('-image')
+        .populate('makeofCar') 
+        .sort([[sortBy, order]])
+        .limit(limit)
+        .exec((err, vehicles) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'The Vehicle is not found in the DB'
+                });
+            }
+            res.json(vehicles);
+        });
+};
+
+
+
+
+/**
+ * list products by search, using price range 
+ */
+  
+exports.searchCar = (req, res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+ 
+    // console.log(order, sortBy, limit, skip, req.body.filters);
+    // console.log("findArgs", findArgs);
+ 
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+ 
+    Vehicle.find(findArgs)
+        .select("-image")
+        //.populate("category")
+        .sort([[sortBy, order]])
+        .skip(skip)
+        .limit(limit)
+        .exec((err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Vehicle is not found"
+                });
+            }
+            res.json({
+                size: data.length,
+                data
+            });
+        });
+};
+
+
+
+
+exports.searchbar = (req, res) => {
+    // create query object to hold search value and category value
+    const query = {};
+    // assign search value to query.name
+    //if (req.query) {
+       // query.make = { $regex: req.query.make, $options: 'i'};
+       // query.make = { $regex: req.query.search, $options: 'i' };
+        // assigne category value to query.category
+        /*if (req.query.category && req.query.category != 'All') {
+            query.category = req.query.category;
+        }*/
+        // find the product based on query object with 2 properties
+        // search and category
+
+        Vehicle.find(req.make, (err, vehicle) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(vehicle);
+        }).select('-photo');
+    //}
+};
+
+
+
+
+
+
+
 
 
 exports.decreaseQuantity = (req, res, next) => {
--- backend/routes/vehicle.js
+++ backend/routes/vehicle.js
@@ -3,13 +3,14 @@ const router = express.Router()
 // import from the controller where the logic is handled for sign-up/out process
 const {requireSignin,isAuth,isAdmin} = require('../controllers/auth');
 // to create a new car
-const {create, vehicleById ,read, remove} = require('../controllers/vehicle');
+const {create, vehicleById ,read, remove,findCar,searchCar,image,searchbar} = require('../controllers/vehicle');
 // import from the controller for customer where the logic is handled for sign-up/out process
 const {customerById} = require('../controllers/customer');
 
 // a page to find the car using id
 router.get('/vehicle/:vehicleId', read);
-
+//get vehicle picture using vehicle id
+router.get("/vehicle/picture/:vehicleId", image);
 //upload a vehicle and image  to db using admin id 
 router.post('/vehicle/create/:customerId',requireSignin,isAuth,isAdmin,create);
 //delete a car using vehicleId and adminId(customer with role=1)
@@ -18,5 +19,13 @@ router.delete('/vehicle/:vehicleId/:customerId',requireSignin,isAuth,isAdmin,rem
 // take the id-param from routes and execute customerById and vehicleById 
 router.param('customerId',customerById)
 router.param('vehicleId',vehicleById)
+
+//get vheicles by make and model 
+router.get("/vehicles", findCar);
+// send the producted filterd by search catagory to the frontend 
+router.post("/vehicles/search", searchCar);
+
+// get the vehicle using the dsearch bar by make, model or year  
+router.post("/vehicles/searchbar", searchbar);
 
 module.exports = router
