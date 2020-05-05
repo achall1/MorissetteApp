a445622be3c66157b3dcaa142dc3d8005435ed11
amanuel <achall1@students.towson.edu>
2020-05-05 18:39:34 -0400

    search/getall/search by price routes done

--- backend/controllers/vehicle.js
+++ backend/controllers/vehicle.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const {errorHandler} = require('../helpers/dbError')
 const Vehicle = require('../models/vehicle')
 
+//find vehicle  by id
 exports.vehicleById =(req,res,next,id) =>
 {
     Vehicle.findById(id).exec((err, vehicle) =>{
@@ -13,13 +14,13 @@ exports.vehicleById =(req,res,next,id) =>
                 error: 'vehicle is not found'
             })
         }
-        // poulate the vehicle from the db using id
+        // poulate the vehicle to the db using id
         req.vehicle = vehicle
         next()
     })
 }
 
-// read vehicle and return
+// find and return vehicle
 exports.read = (req, res) =>
 {
     // set image undefined because its to large to send to client directly
@@ -27,12 +28,24 @@ exports.read = (req, res) =>
     return res.json(req.vehicle)
 }
 
+
+//retrun vehicle image  to frontend 
+// example query http://localhost:8000/api/vehicle/picture/5e7408ced3ae6b3b0cb30730
+exports.image = (req, res, next) => {
+    if (req.vehicle.image.data) {
+        res.set('Content-Type',req.vehicle.image.contentType);//"image/jpeg; charset=UTF-8" 
+        return res.send(req.vehicle.image.data);
+    }
+    next();
+};
+
+
 // delete a vehicle using adminiD and vehicle id
 exports.remove = (req, res) =>
 {
     let vehicle = req.vehicle
 
-    vehicle.remove((err,deletedVehicle) =>{
+    vehicle.remove((err) =>{
         if(err)
         {
             return res.status(400).json({
@@ -44,10 +57,8 @@ exports.remove = (req, res) =>
    
 }
 
-
-
-
 // used to handle form data and image coming from client 
+//create a vehicle and upload image 
 exports.create = (req,res) =>
 {
     let form = new formidable.IncomingForm()
@@ -95,3 +106,137 @@ if(!make || !model|| !mileage||  !price|| !vin|| !invintoryCount)
         })
     })
 }
+
+
+
+
+
+/**
+    search vehicle using price range you get from the body 
+ */
+  
+exports.searchByPrice = (req, res) => {
+    
+   // let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    //let skip = parseInt(req.body.skip);
+    let findArgs = {};
+    
+ 
+   for (let key in req.body.filters) {
+
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            }else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    } 
+//find the vehicle in db using the price range 
+    Vehicle.find(findArgs)
+        .select("-image")
+        .sort()
+       // .skip(skip)
+      //.limit(limit)
+        .exec((err, vehicle) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Vehicle is not found"
+                });
+            }
+            res.json({
+                size: vehicle.length,
+                vehicle
+            });
+        });
+};
+
+
+
+// this method will search and return vehicle items found in the db
+// the param for this query is either a vehicle model or make or year
+// example  serching for model http://localhost:8000/api/vehicles/searchbar?model=civic
+// example  serching for make http://localhost:8000/api/vehicles/searchbar?make=toytota
+// example  serching for year http://localhost:8000/api/vehicles/searchbar?year=2020
+
+exports.searchbar = (req, res) => {
+    // create query object to hold search value and category value
+     const query = {};
+    // assign search value to query.name
+     if (req.query.make) {
+        query.make = { $regex: req.query.make, $options: 'i'};
+     }
+     else if(req.query.model)
+     {
+        query.model = { $regex: req.query.model, $options: 'i'};
+     }
+     else if(req.query.year)
+     {
+        query.year = { $regex: req.query.year, $options: 'i'};
+     }
+
+        Vehicle.find( query ) 
+            .select('-image') // deselect the image since it takes time and space to query
+            .exec((err, vehicle) => {
+           
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err + "vehicle year,make or model has no Match")
+                });
+            }
+            res.json({ vehicle,size:vehicle.length});
+        })
+};
+
+
+/**
+ * find all cars and return them by order using model/make/year or any other fiels
+ * the query url is like below
+ * http://localhost:8000/api/vehicles/findall
+ */
+
+exports.findallvehicles = (req, res) => {
+    
+   let order = req.query.order = 'asc';
+    Vehicle.find(req.id)
+        .select('-image') 
+         .sort(order)
+        .limit(100)
+        .exec((err, vehicles) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'The Vehicles are not found in the DB'
+                });
+            }
+            res.json(vehicles);
+        });
+};
+
+
+
+
+/* this function needs testing */
+exports.decreaseQuantity = (req, res, next) => {
+    let bulkOps = req.body.order.products.map(item => {
+        return {
+            updateOne: {
+                filter: { _id: item._id },
+                update: { $inc: { invintoryCount: -item.count } }
+            }
+        };
+    });
+
+    Product.bulkWrite(bulkOps, {}, (error, products) => {
+        if (error) {
+            return res.status(400).json({
+                error: 'Could not update product'
+            });
+        }
+        next();
+    });
+};
--- backend/routes/vehicle.js
+++ backend/routes/vehicle.js
@@ -2,21 +2,37 @@ const express = require('express')
 const router = express.Router()
 // import from the controller where the logic is handled for sign-up/out process
 const {requireSignin,isAuth,isAdmin} = require('../controllers/auth');
-// to create a new car
-const {create, vehicleById ,read, remove} = require('../controllers/vehicle');
+// to create a new car,search,read,get picture 
+
+const {create, vehicleById ,read, remove, image, searchByPrice,searchbar,findallvehicles} = require('../controllers/vehicle');
+
 // import from the controller for customer where the logic is handled for sign-up/out process
 const {customerById} = require('../controllers/customer');
 
 // a page to find the car using id
 router.get('/vehicle/:vehicleId', read);
 
-//used to post a car to db
+//get vehicle picture using vehicle id
+router.get("/vehicle/picture/:vehicleId", image);
+
+//upload a vehicle and image  to db using admin id 
 router.post('/vehicle/create/:customerId',requireSignin,isAuth,isAdmin,create);
+
 //delete a car using vehicleId and adminId(customer with role=1)
 router.delete('/vehicle/:vehicleId/:customerId',requireSignin,isAuth,isAdmin,remove);
 
 // take the id-param from routes and execute customerById and vehicleById 
 router.param('customerId',customerById)
 router.param('vehicleId',vehicleById)
+
+// get the vehicle using the a search bar or something in the frontend 
+// it querys by make, model or year  
+router.post("/vehicles/searchbar", searchbar);
+
+// send the vehicles filterd by price  to the frontend 
+router.post("/vehicles/searchprice", searchByPrice);
+
+//find an retrun all vehicle sorted by the model name in order
+router.get("/vehicles/findall", findallvehicles);
 
 module.exports = router
